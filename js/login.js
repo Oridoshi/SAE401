55636e7377c0a3795c36a7b786650f1fdd2159dc
VicenteHugo <hugovicente965@gmail.com>
2024-04-02 18:40:57 +0200

    Sauvegarde utilisateur en cour utilisation
    Co-authored-by: Titouan Bertaux <TitouanBertaux@users.noreply.github.com>
    Co-authored-by: Dunet Tom <Oridoshi@users.noreply.github.com>
    Co-authored-by: baptiste.hay@hotmail.fr <baptiste.hay@hotmail.fr>

--- js/login.js
+++ js/login.js
@@ -1,46 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
-	var overlay = document.getElementById('overlay');
-	var modal = document.getElementById('loginModal');
-	
-	fetch("http://192.168.1.17:8000/verifLogin.php").then(response =>{
-		if(response.ok)
-			return response.json();
-	}).then(data => {
-		console.log(data);
-		if(!data) {
-			overlay.classList.remove('hidden');
-			modal.classList.remove('hidden');
+	if(sessionStorage.getItem('login') !== null) {
+		console.log('Utilisateur connecté :', sessionStorage.getItem('login'));
+	}
+	else {
+		var overlay = document.getElementById('overlay');
+		var modal = document.getElementById('loginModal');
+		overlay.classList.remove('hidden');
+		modal.classList.remove('hidden');
 
-			var loginForm = document.getElementById('loginForm');
-			loginForm.addEventListener('submit', function(event) {
-				event.preventDefault();
-				var username = document.getElementById('username').value;
-				var password = document.getElementById('password').value;
+		var loginForm = document.getElementById('loginForm');
+		loginForm.addEventListener('submit', function(event) {
+			event.preventDefault();
+			var username = document.getElementById('username').value;
+			var password = document.getElementById('password').value;
 
-				if (username.trim() === '' || password.trim() === '') {
-					alert('Veuillez saisir un nom d\'utilisateur et un mot de passe.');
-				} else {
-					fetch("http://192.168.1.17:8000/verifLogin.php", {
-						method: 'POST',
-						body: JSON.stringify({ login: username, mdp: password }),
-						headers: {
-							'Content-Type': 'application/json'
-						}
-					})
-					.then(response => response.json())
-					.then(data => {
-						if(data) {
-							overlay.classList.add('hidden');
-							modal.classList.add('hidden');
-						} else {
-							alert('Nom d\'utilisateur ou mot de passe incorrect.');
-						}
-					})
-					.catch(error => {
-						console.error('Erreur:', error);
-					});
-				}
-			});
-		}
-	});
+			if (username.trim() === '' || password.trim() === '') {
+				alert('Veuillez saisir un nom d\'utilisateur et un mot de passe.');
+			} else {
+				fetch("http://192.168.1.17:8000/verifLogin.php", {
+					method: 'POST',
+					body: JSON.stringify({ login: username, mdp: password }),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				})
+				.then(response => response.json())
+				.then(data => {
+					if(data) {
+						sessionStorage.setItem('login', username);
+						overlay.classList.add('hidden');
+						modal.classList.add('hidden');
+					} else {
+						alert('Nom d\'utilisateur ou mot de passe incorrect.');
+					}
+				})
+				.catch(error => {
+					console.error('Erreur:', error);
+				});
+			}
+		});
+	}
 });
