8834c2bf77f5b1994afccbdcfae526d6d4052516
Dunet Tom <116595007+Oridoshi@users.noreply.github.com>
2024-04-03 11:56:31 +0200

    Traitement fichier - N'insère pas dans le bas de donnée encore mais lit et traite les fichier passé en paramettre - maque traitement des "creationXX.php" qui n'on pas était modifier/corriger

--- php/upload.php
+++ php/upload.php
@@ -1,4 +1,11 @@
 <?php
+
+include_once("lecureFIchier.php");
+include_once("creationEtudiant.php");
+include_once("creationResultat.php");
+include_once("creationCompetence.php");
+include_once("creationModules.php");
+
 // Vérifier si un fichier a été envoyé
 if(isset($_FILES['file'])) {
     $target_dir = "../donnees/"; // Répertoire où vous souhaitez enregistrer les fichiers
@@ -6,10 +13,37 @@ if(isset($_FILES['file'])) {
 
     // Déplacer le fichier vers le répertoire de destination
     if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        traitementFichier($target_file, $_POST["source"]);
         echo "Le fichier " . htmlspecialchars(basename($_FILES["file"]["name"])) . " a été téléchargé.";
     } else {
         echo "Désolé, une erreur s'est produite lors du téléchargement du fichier.";
     }
 } else {
     throw new Exception("Aucun fichier n'a été envoyé.");
+}
+
+
+/** CREATION DES OBJETS
+ * - Etudiant   : utilise le fichier moyenne ou jury
+ * - Resultat   : utilise le fichier moyenne
+ * - Competence : utilise le fichier jury
+ * - Modules    : utilise le fichier moyenne
+ */
+
+function traitementFichier($file, $source) {
+    $regexJury  = "/(jury)/";
+    $regexCoeff = "/(coeff)/";
+
+    if(preg_match($regexJury, $source)) {
+        $fichierLue = lectureFichier($file);
+        creationEtudiant($fichierLue, $_POST["promo"]);
+        creationCompetence($fichierLue);
+    } else if(preg_match($regexCoeff, $source)) {
+        $source = "coeff";
+    } else {
+        $fichierLue = lectureFichier($file);
+        creationEtudiant($fichierLue, $_POST["promo"]);
+        creationResultat($fichierLue);
+        creationModules($fichierLue);
+    }
 }
